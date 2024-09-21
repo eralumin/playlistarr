2e0de822b7bd6d9941fd462b20283a179e689ad8
Erin Marilleau <erin.marilleau@gmail.com>
2024-09-21 02:55:54 +0200

    fix get env var

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,10 @@ import os
 def get_env_variable(var_name, default=None):
     value = os.getenv(var_name)
 
-    if not default and not value:
+    if value is None or value == '':
+        if default is not None:
+            return default
+
         raise EnvironmentError(f"The environment variable {var_name} is not set. Please configure it properly.")
 
     return value
